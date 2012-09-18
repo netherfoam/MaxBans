19aec89980a504175e2d6f189d6c00f2a0f2d225
Netherfoam <i.got.burnt@gmail.com>
2012-09-18 17:30:43 +1000

    Additional comments

--- MaxBans/src/org/maxgamer/maxbans/MaxBans.java
+++ MaxBans/src/org/maxgamer/maxbans/MaxBans.java
@@ -28,35 +28,59 @@ public class MaxBans extends JavaPlugin{
         private Database db;
         
 	public void onEnable(){
+		
+		/*
+		 * Generates files for the first run
+		 */
 		if(!this.getDataFolder().exists()){
 			this.getDataFolder().mkdir();
 		}
 		File configFile = new File(this.getDataFolder(), "config.yml");
 		
 		if(!configFile.exists()){
+			//Saves config.yml from inside the plugin
+			//into the plugins directory folder
 			this.saveResource("config.yml", false);
 		}
-		//Is this necessary?
+		
+		/*
+		 * Reloads the config from disk.
+		 * Normally this is done before onEnable()
+		 * anyway, but, if we do /plugman reload MaxBans,
+		 * this makes it friendlier.
+		 */
 		this.reloadConfig();
 		
+		//The database for bans
 		db = new Database(this, new File(this.getDataFolder(), "bans.db"));
 		
+		//BanManager
 		banManager = new BanManager(this);
 		
+		//Listeners for chat (mute) and join (Ban)
 		this.chatListener = new ChatListener(this);
 		this.joinListener = new JoinListener(this);
 	
+		//Register listeners
         Bukkit.getServer().getPluginManager().registerEvents(this.chatListener, this);
         Bukkit.getServer().getPluginManager().registerEvents(this.joinListener, this);
-        
     }
+	//TODO: Write onDisable method
 	public void onDisable(){
 		
 	}
+	/**
+	 * Returns the ban manager for banning and checking bans and mutes.
+	 * @return the ban manager for banning and checking bans and mutes.
+	 */
     public BanManager getBanManager() {
         return banManager;
     }
         
+    /**
+     * Returns the raw database for storing data and loading into the cache.
+     * @return the raw database for storing data and loading into the cache.
+     */
     public Database getDB(){
     	return db;
     }
