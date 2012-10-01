df8e871b5254f02af0cc1f884a06bd2287a2f842
Netherfoam <i.got.burnt@gmail.com>
2012-10-01 11:47:18 +1000

    Unmute command

---
+++ MaxBans/src/org/maxgamer/maxbans/commands/UnMute.java
@@ -0,0 +1,38 @@
+package org.maxgamer.maxbans.commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.maxgamer.maxbans.MaxBans;
+import org.maxgamer.maxbans.banmanager.Mute;
+
+public class UnMute implements CommandExecutor{
+    private MaxBans plugin;
+    public UnMute(MaxBans plugin){
+        this.plugin = plugin;
+    }
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		String usage = ChatColor.RED + "Usage: /mute <player> <reason>";
+		
+		if(args.length > 0){
+			String name = args[0];
+			
+			Mute mute = plugin.getBanManager().getMute(name);
+			if(mute != null){
+				plugin.getBanManager().unmute(name);
+				sender.sendMessage(ChatColor.GREEN + "Unmuted " + name);
+				return true;
+			}
+			else{
+				sender.sendMessage(ChatColor.GREEN + name + " is not muted.");
+				return true;
+			}
+		}
+		else{
+			sender.sendMessage(usage);
+			return true;
+		}
+	}
+}
