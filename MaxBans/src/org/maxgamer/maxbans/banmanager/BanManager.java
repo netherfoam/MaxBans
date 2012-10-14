37f1e15f4d3868cd0303cce06f9cda8513a66e21
Netherfoam <i.got.burnt@gmail.com>
2012-10-14 13:23:57 +1000

    Unused iterator

--- MaxBans/src/org/maxgamer/maxbans/banmanager/BanManager.java
+++ MaxBans/src/org/maxgamer/maxbans/banmanager/BanManager.java
@@ -6,7 +6,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 
 import org.bukkit.Bukkit;
