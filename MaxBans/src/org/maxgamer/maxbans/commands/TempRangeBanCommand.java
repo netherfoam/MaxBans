ab469710f8d71c2c0c43c7be78a012ddaef63b48
netherfoam <admin@maxgamer.org>
2013-02-19 13:50:09 +1000

    Finished temprangeban command

--- MaxBans/src/org/maxgamer/maxbans/commands/TempRangeBanCommand.java
+++ MaxBans/src/org/maxgamer/maxbans/commands/TempRangeBanCommand.java
@@ -16,16 +16,16 @@ public class TempRangeBanCommand extends CmdSkeleton{
 
 	@Override
 	public boolean run(CommandSender sender, Command cmd, String label, String[] args) {
-		String banner = Util.getName(sender);
-		boolean silent = Util.isSilent(args);
-		long expires = Util.getTime(args);
-		if(expires <= 0){
+		if(args.length < 3){
 			sender.sendMessage(getUsage());
 			return true;
 		}
-		String reason = Util.buildReason(args);
+		
+		String banner = Util.getName(sender);
+		boolean silent = Util.isSilent(args);
 		String[] ips = args[0].split("-");
 		
+		sender.sendMessage(ips);
 		if(ips.length == 1 && ips[0].contains("*")){
 			//If they supplied us with /rangeban 127.0.0.*, then we know its actually 127.0.0.0-127.0.0.255.
 			ips = new String[]{ips[0].replace('*', '0'), ips[0].replace("*", "255")};
@@ -45,10 +45,18 @@ public class TempRangeBanCommand extends CmdSkeleton{
 		IPAddress start = new IPAddress(ips[0]);
 		IPAddress end = new IPAddress(ips[1]);
 		
+		long expires = Util.getTime(args) + System.currentTimeMillis();
+		if(expires <= 0){
+			sender.sendMessage(getUsage());
+			return true;
+		}
+		String reason = Util.buildReason(args);
+		
 		TempRangeBan rb = new TempRangeBan(banner, reason, System.currentTimeMillis(), expires, start, end);
 		RangeBan overlap = plugin.getBanManager().getRanger().ban(rb);
 		if(overlap == null){
-			plugin.getBanManager().announce(Formatter.secondary + banner + Formatter.primary + " banned " + Formatter.secondary + rb.toString() + Formatter.primary + ". Reason: '" + Formatter.secondary + reason + Formatter.primary + "' Remaining: " + Util.getTimeUntil(rb.getExpires()), silent, sender);
+			sender.sendMessage(rb.getExpires() + "v" + System.currentTimeMillis());
+			plugin.getBanManager().announce(Formatter.secondary + banner + Formatter.primary + " banned " + Formatter.secondary + rb.toString() + Formatter.primary + ". Reason: '" + Formatter.secondary + reason + Formatter.primary + "' Remaining: "+ Formatter.secondary + Util.getTimeUntil(rb.getExpires()), silent, sender);
 		}
 		else{
 			sender.sendMessage(ChatColor.RED + "That RangeBan overlaps an existing one! (" + overlap.toString() + ")");
