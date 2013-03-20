cc7a182728f84b791d8d99c9d27d95cbb91ee1f3
netherfoam <admin@maxgamer.org>
2013-03-20 14:55:35 +1000

    Try to fix reconnection issues...

--- MaxBans/src/org/maxgamer/maxbans/database/MySQL.java
+++ MaxBans/src/org/maxgamer/maxbans/database/MySQL.java
@@ -3,25 +3,27 @@ package org.maxgamer.maxbans.database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 import org.maxgamer.maxbans.MaxBans;
 
 public class MySQL implements DatabaseCore{
 	private MaxBans plugin;
 	private String url;
-	private String user;
-	private String pass;
-	
+	/** The connection properties... user, pass, autoReconnect.. */
+	private Properties info;
+	/** The actual connection... possibly expired. */
 	private Connection connection;
 	
-	public MySQL(MaxBans plugin, String url, String user, String pass){
-		this.plugin = plugin;
-		this.url = url;
-		this.user = user;
-		this.pass = pass;
-	}
 	public MySQL(MaxBans plugin, String host, String user, String pass, String database, String port){
-		this(plugin, "jdbc:mysql://"+host+":"+port+"/"+database+"?autoReconnect=true", user, pass);
+		info = new Properties();
+		info.put("autoReconnect", "true");
+		info.put("user", user);
+		info.put("password", pass);
+		info.put("useUnicode", "true");
+		info.put("characterEncoding", "utf8");
+		this.url = "jdbc:mysql://"+host+":"+port+"/"+database;
+		this.plugin = plugin;
 	}
 	
 	
@@ -37,7 +39,8 @@ public class MySQL implements DatabaseCore{
 				return this.connection;
 			}
 			else{
-				this.connection = DriverManager.getConnection(this.url, user, pass);
+				//this.connection = DriverManager.getConnection(this.url, user, pass);
+				this.connection = DriverManager.getConnection(this.url, info);
 				return this.connection;
 			}
 		}
